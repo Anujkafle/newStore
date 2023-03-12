a18356210e09d381a44e3bac9c79795599999f77
Arjan kc <arjankc@gmail.com>
2023-03-12 22:40:57 +0545

    eSewa integrated
    Please read documentation, use 123456 as OTP
    https://developer.esewa.com.np/#/epay

--- NewEcommerce/usersarea/payment.php
+++ NewEcommerce/usersarea/payment.php
@@ -47,7 +47,19 @@
                 <?php 
                 $total=$_GET["total"];
                 ?>
-                <a href="esewa.php?total=<?php echo $total ?>" class="m-5 p-3"><img src="../imgs/Khalti.jpg" alt="esewa"></a>  
+                <a href="esewa.php?total=<?php echo $total ?>" class="m-5 p-3"></a>
+                <form action="https://uat.esewa.com.np/epay/main" method="POST">
+                <input value="<?php echo $total ?>" name="tAmt" type="hidden">
+                <input value="<?php echo $total ?>" name="amt" type="hidden">
+                <input value="0" name="txAmt" type="hidden">
+                <input value="0" name="psc" type="hidden">
+                <input value="0" name="pdc" type="hidden">
+                <input value="EPAYTEST" name="scd" type="hidden">
+                <input value="ee2c3ca1-696b-4cc5-a6be-2c40d929d45" name="pid" type="hidden">
+                <input value="http://merchant.com.np/page/esewa_payment_success?q=su" type="hidden" name="su">/IF SUCCESS GO HERE
+                <input value="http://merchant.com.np/page/esewa_payment_failed?q=fu" type="hidden" name="fu">//IF FAIL GO HERE
+                <input type= "image" src="../imgs/esewa.jpg" value="Submit" width="40%">
+                </form>  
             </div>
             <div class="col-md-6">
                 <a href="order.php?user_id=<?php echo $user_id ?>"  class="text-decoration-none text-center"><h2>Pay offline</h2></a>
